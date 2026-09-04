4928c2091498c95ac8797f511193ed3624f0680c
gojolover91156-ux <gojolover91156@gmail.com>
2026-09-04 14:14:40 +0530

    Update status.js

--- status.js
+++ status.js
@@ -1,16 +1,35 @@
+const statuses = [
+  "ʙᴇ ᴀ ᴍᴀɴ, ɴᴏᴛ ᴀ ʙᴀsᴛᴀʀᴅ.",
+  "ᴅᴏɴ'ᴛ ᴛʀᴜsᴛ ᴇᴠᴇʀʏᴏɴᴇ.",
+  "ᴋᴇᴇᴘ ɢᴏɪɴɢ.",
+  "ᴋɴᴏᴡ ʏᴏᴜʀ ᴡᴏʀᴛʜ."
+];
+
 async function setStatus(client) {
-  await client.updatePresence(0, {
-    since: null,
-    activities: [
-      {
-        name: "custom",
-        type: 4,
-        state: "ʙᴇ ᴀ ᴍᴀɴ, ɴᴏᴛ ᴀ ʙᴀsᴛᴀʀᴅ."
-      }
-    ],
-    status: "idle",
-    afk: false
-  });
+  let index = 0;
+
+  const update = async () => {
+    await client.updatePresence(0, {
+      since: null,
+      activities: [
+        {
+          name: "custom",
+          type: 4,
+          state: statuses[index]
+        }
+      ],
+      status: "idle",
+      afk: false
+    });
+
+    index = (index + 1) % statuses.length;
+  };
+
+  // Set first status immediately
+  await update();
+
+  // Change status every 10 seconds
+  setInterval(update, 10_000);
 }
 
 module.exports = { setStatus };
